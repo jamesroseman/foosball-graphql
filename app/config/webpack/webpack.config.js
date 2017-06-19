e147ead332673ccf6246f6da4f097ed12e55d7da
James Roseman <jroseman@twitter.com>
2017-06-19 10:05:10 -0400

    Backend and frontend servers now run on different ports

--- app/config/webpack/webpack.config.js
+++ app/config/webpack/webpack.config.js
@@ -1,39 +1,43 @@
-const getBabelRelayPlugin = require('babel-relay-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:5000',
+      'webpack/hot/only-dev-server',
+      './src/index.tsx'
+    ],
+
     output: {
-        filename: "bundle.js",
-        path: __dirname + "/../../public/dist"
+        filename: 'bundle.js',
+        publicPath: path.join(__dirname, '../../public/'),
+        path: path.join(__dirname, '../../public/dist')
     },
 
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: 'source-map',
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
     },
 
     module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            {
-              test: /\.tsx?$/,
-              loader: "awesome-typescript-loader",
-              query: {
-                useBabel: true,
-                useCache: true,
-                babelOptions: {
-                  plugins: [ 'relay' ],
-                  presets : [ 'es2015', 'react' ]
-                }
-              }
-            },
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loaders: ['babel-loader', 'ts-loader']
+        },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader'
+        }
+      ]
     },
 
     // When importing a module whose path matches one of the following, just
@@ -41,7 +45,12 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+        'react': 'React',
+        'react-dom': 'ReactDOM'
     },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new WebpackNotifierPlugin({ alwaysNotify: true })
+    ]
 };
---
+++ app/dist/containers/App.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+exports.default = function () { return React.createElement("div", null, "Hello world"); };
+//# sourceMappingURL=App.js.map
